a5cbae26e0b3aecb11913d8cad09792ae1b3b236
morthenn <minerty@gmail.com>
2018-04-29 17:15:45 +0200

    fixed post, put, minor things

--- src/main/java/com/anesthesia/anesthesiamanager/service/PatientService.java
+++ src/main/java/com/anesthesia/anesthesiamanager/service/PatientService.java
@@ -1,9 +1,16 @@
 package com.anesthesia.anesthesiamanager.service;
 
 import com.anesthesia.anesthesiamanager.model.Patient;
+import com.anesthesia.anesthesiamanager.model.User;
 import com.anesthesia.anesthesiamanager.repository.PatientRepository;
+import com.anesthesia.anesthesiamanager.repository.UserRepository;
+import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 
 /**
@@ -15,15 +22,29 @@ public class PatientService {
     @Autowired
     private PatientRepository patientRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public Patient getPatientForUser(Long patientId) {
         return patientRepository.findOne(patientId);
     }
 
-    public Patient addPatient(Patient patient) {
-        return patientRepository.save(patient);
+    public Patient addPatient(Patient patient, Long userId) {
+        if (!patientRepository.exists(patient.getId())) {
+            User user = userRepository.findOne(userId);
+            patient.setUser(user);
+            patientRepository.save(patient);
+            return patient;
+        }
+        return null;
     }
 
-    public boolean isUserExist(Patient patient) {
-        return patientRepository.exists(patient.getId());
+    public Patient updatePatient(Long userId,Long patientId, Patient patientUpdateBody) {
+        Patient existingPatient = patientRepository.findOne(patientId);
+        if (existingPatient == null) return null;
+        patientUpdateBody.setId(patientId);
+        patientUpdateBody.setUser(existingPatient.getUser());
+        patientRepository.save(patientUpdateBody);
+        return patientUpdateBody;
     }
 }
